ea2c1024ffaea80011b5e93e73cb7b04d225f8fb
vanhoa690 <vanhoa690@gmail.com>
2026-03-23 09:21:19 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,45 @@
-import { Layout, Menu } from "antd";
-import { Header } from "antd/es/layout/layout";
+import { Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom";
-import "antd/dist/reset.css";
 
 function App() {
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Header>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1">
-            <Link to="/">WEB2091</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/">List</Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Link to="/">Add</Link>
-          </Menu.Item>
-        </Menu>
-      </Header>
-      <main style={{ padding: "0 100px" }}>
-        <h1 style={{ textAlign: "center", margin: 10 }}>Bài Làm</h1>
-      </main>
-    </Layout>
+    <>
+      <nav className="bg-blue-600 text-white shadow">
+        <div className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between">
+          <Link to="#" className="text-xl font-semibold">
+            <strong>WEB2091 App</strong>
+          </Link>
+
+          <div className="hidden md:flex items-center space-x-8">
+            <Link to="#" className="hover:text-gray-200">
+              Trang chủ
+            </Link>
+            <Link to="/list" className="hover:text-gray-200">
+              Danh sách
+            </Link>
+            <Link to="/add" className="hover:text-gray-200">
+              Thêm mới
+            </Link>
+          </div>
+
+          <div className="hidden md:flex items-center space-x-6">
+            <Link to="#" className="hover:text-gray-200">
+              Đăng nhập
+            </Link>
+            <Link to="#" className="hover:text-gray-200">
+              Đăng ký
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      {/* MAIN CONTENT */}
+      <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
+        <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB2091</h1>
+      </div>
+
+      <Toaster />
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -13,5 +14,5 @@ createRoot(document.getElementById("root")!).render(
         <App />
       </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
